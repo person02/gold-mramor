9f68542ce96e32d43b5b1a47b2ce2e989fda9da9
person02 <person_0505@mail.ru>
2026-01-05 17:11:02 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const burger=document.querySelector('.burger')
-const menu=document.querySelector('.menu')
-
-burger?.addEventListener('click',()=>{
-  menu.style.display=
-    menu.style.display==='block'?'none':'block'
-})
